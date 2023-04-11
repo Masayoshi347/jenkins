c3f0c832c71e5a0249010da9f5d1b683323e1d2c
Charruyer Guillaume <90193223+Masayoshi347@users.noreply.github.com>
2023-04-11 14:25:40 +0200

    Add files via upload

---
+++ Jenkins.py
@@ -0,0 +1,15 @@
+# file: main.py
+
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def main():
+    print("1 + 2 = ", add_numbers(1, 2))
+    print("3 * 4 = ", multiply_numbers(3, 4))
+
+if __name__ == '__main__':
+    main()
+
